138f1ec66b760fde892a01cdddf9f560714b7bc5
franwe <franziska.wehrmann@gmail.com>
2020-09-27 16:43:32 +0200

    Remove tau, since tau is fix

--- util/smoothing_f.py
+++ util/smoothing_f.py
@@ -2,33 +2,27 @@ import numpy as np
 from scipy.stats import norm
 
 # ----------------- with tau ------- Rookley + Haerdle (Applied Quant. Finance)
-def gaussian_kernel(M, m, h_m, T, t, h_t):
-    u_m = (M-m)/h_m
-    u_t = (T-t)/h_t
-    return norm.cdf(u_m) * norm.cdf(u_t)
+def gaussian_kernel(M, m, h):
+    u_m = (M-m)/h
+    return norm.cdf(u_m)
 
 
-def epanechnikov(M, m, h_m, T, t, h_t):
-    u_m = (M-m)/h_m
-    u_t = (T-t)/h_t
-    return 3/4 * (1-u_m)**2 * 3/4 * (1-u_t)**2
+def epanechnikov(M, m, h):
+    u_m = (M-m)/h
+    return 3/4 * (1-u_m)**2
 
 
-def smoothing_rookley(df, m, t, h_m, h_t, kernel=gaussian_kernel):
+def smoothing_rookley(df, m, h, kernel=gaussian_kernel):
     M = np.array(df.M)
-    T = np.array(df.tau)
     y = np.array(df.iv)
     n = df.shape[0]
 
     X1 = np.ones(n)
     X2 = M - m
     X3 = (M-m)**2
-    X4 = T-t
-    X5 = (T-t)**2
-    X6 = X2*X4
-    X = np.array([X1, X2, X3, X4, X5, X6]).T
+    X = np.array([X1, X2, X3]).T
 
-    ker = kernel(M, m, h_m, T, t, h_t)
+    ker = kernel(M, m, h)
     W = np.diag(ker)
 
     XTW = np.dot(X.T, W)
@@ -40,7 +34,7 @@ def smoothing_rookley(df, m, t, h_m, h_t, kernel=gaussian_kernel):
 
 
 
-def local_polynomial(df, tau, h_m, h_t=0.05, gridsize=50, kernel='epak'):
+def local_polynomial(df, h, gridsize=50, kernel='epak'):
 
     if kernel=='epak':
         kernel = epanechnikov
@@ -56,7 +50,7 @@ def local_polynomial(df, tau, h_m, h_t=0.05, gridsize=50, kernel='epak'):
 
     sig = np.zeros((num, 3))
     for i, m in enumerate(M):
-        sig[i] = smoothing_rookley(df, m, tau, h_m, h_t, kernel)
+        sig[i] = smoothing_rookley(df, m, h, kernel)
 
     smile = sig[:, 0]
     first = sig[:, 1]
@@ -81,9 +75,9 @@ RndData = RndDataClass(data_path + 'trades_clean.csv', cutoff=x)
 HdData = HdDataClass(data_path + 'BTCUSDT.csv')
 
 df_tau = RndData.filter_data("2020-03-11", 9)
-tau = df_tau.tau.iloc[0]
 
-smile, first, second, M, S, K = local_polynomial(df_tau, tau, h_m=0.1, h_t=0.1,
+smile, first, second, M, S, K = local_polynomial(df_tau, h=0.1,
                                                  gridsize=140, kernel='epak')
+
 plt.scatter(df_tau.M, df_tau.iv)
 plt.plot(M, smile, 'r')
