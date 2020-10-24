356b20803c0b82913ef7defad6663fc9745aa098
franwe <franziska.wehrmann@gmail.com>
2020-10-24 19:53:22 +0200

    check data from Julian's upload

--- util/connect_server.py
+++ util/connect_server.py
@@ -1,6 +1,7 @@
 from sshtunnel import SSHTunnelForwarder
 import pymongo
 import pprint
+from datetime import datetime
 
 # ssh -i IRTG franziska_wehrmann@35.205.115.90
 
@@ -22,8 +23,25 @@ client = pymongo.MongoClient("127.0.0.1", server.local_bind_port)
 db = client[MONGO_DB]
 pprint.pprint(db.collection_names())
 
-coll = db[MONGO_COLLECTION]
+collection = db[MONGO_COLLECTION]
 
-coll.find_one()
+# coll.find_one()
+
+cursor = collection.aggregate(
+    [
+        {
+            "$group": {
+                "_id": None,
+                "max": {"$max": "$timestamp"},
+                "min": {"$min": "$timestamp"},
+            }
+        }
+    ]
+)
+print(list(cursor))
+
+ts = [1583354259064, 1601122235251]
+for t in ts:
+    print(datetime.fromtimestamp(t / 1000))
 
 server.stop()
